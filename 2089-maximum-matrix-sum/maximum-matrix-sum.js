3bd90bbce821b59c6a563f7b1a911e06e95e6f63
Vinay Somawat <vinaysomawat40@gmail.com>
2026-01-05 16:39:48 +0530

    Time: 13 ms (40.00%) | Memory: 66.4 MB (30.00%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxMatrixSum = function(matrix) {
+    let count = 0;
+    let min = Infinity;
+    let sum=0;
+    matrix.forEach(item => {
+        item.forEach(temp => {
+            if(temp<0) count++;
+            min = Math.min(min, Math.abs(temp));
+            sum += Math.abs(temp);
+        })
+    });
+    console.log(sum, min);
+    if(count%2==0) return sum;
+    else return sum-2*min;
+};
